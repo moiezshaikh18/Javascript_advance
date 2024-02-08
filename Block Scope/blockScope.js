4a539b42023152a89e04ed1ae64f7be6069a46b2
mueej <v-mueej@fieldera.com>
2024-02-08 13:12:32 +0530

    Function and Objects

--- Block Scope/blockScope.js
+++ Block Scope/blockScope.js
@@ -79,4 +79,183 @@
 
 // let counter = initCounter();
 
-// console.log(counter(), counter());
+// console.log(counter());
+
+// IIFE
+
+// (function () {
+//   var x = 1;
+// })();
+
+// Currying Function
+
+// function op(operation) {
+//   return function (a) {
+//     return function (b) {
+//       return operation === "add" ? a + b : a - b;
+//     };
+//   };
+// }
+
+// const op = (operation) => (a) => (b) => operation === "add" ? a + b : a - b;
+
+// console.log(op("add")(10)(9));
+
+// let log = (time) => (type) => (message) =>
+//   `At ${time}: severity ${type} ${message}  `;
+
+// console.log(log(new Date())("error")("Power Not Sufficient"));
+
+///Object Reference
+
+// let person = { name: "john" };
+// let human = person;
+
+// person;
+// human;
+
+// person = { name: "wick" };
+
+// person;
+// human;
+
+// let person = {
+//   name: "John",
+//   address: {
+//     city: "pune",
+//     state: "maharashtra",
+//   },
+// };
+
+//Copying of object
+
+// let person = {
+//   name: "John",
+//   address: {
+//     city: "pune",
+//     state: "maharashtra",
+//   },
+// };
+
+//Using Object Assign
+
+// let obj1 = {};
+
+// Object.assign(obj1, person);
+
+// obj1;
+
+//Using Spread Operators
+
+// let obj1 = { ...person };
+
+// obj1;
+
+//Using Deep Copy
+
+// let obj1 = JSON.stringify(person);
+
+// obj1;
+
+// let o = JSON.parse(obj1);
+
+// o;
+
+// Using Structured Clone
+
+// let obj1 = structuredClone(person);
+
+// obj1;
+
+// This Keyword and Methods
+
+// let person = {
+//   name: "John",
+//   sayHi: function () {
+//     return "hi" + this.name;
+//   },
+//   address: {
+//     city: "pune",
+//     state: "maharashtra",
+//   },
+// };
+
+// person.sayHi();
+
+//Symbol Data Type
+
+// let person = {
+//   0: "John",
+//   sayHi: function () {
+//     return "hi" + this.name;
+//   },
+// };
+
+// let id = Symbol("id");
+
+// let person = {
+//   [id]: "John",
+//   sayHi: function () {
+//     return "hi" + this.name;
+//   },
+// };
+
+// person;
+
+//Function as objects
+
+// sayHi();
+// function sayHi(greet) {
+//   return greet;
+// }
+
+// sayHi("john");
+
+// sayHi.x = 1;
+
+// function Person(name) {
+//   this.name = name;
+// }
+
+// const p = new Person("name");
+// p;
+
+// const funcEx = function fx(user) {
+//   if (user) {
+//     return "hello" + "  " + user;
+//   } else {
+//     return fx("undefined");
+//   }
+// };
+
+// let b = funcEx();
+// b;
+
+//Decorators Pattern
+
+// function heavy(x) {
+//   console.log(x + ":heavy");
+//   return x + ":heavy";
+// }
+
+// function memoizer(fx) {
+//   let map = new Map(); // hashmap
+
+//   return function (x) {
+//     if (map.has(x)) {
+//       return map.get(x);
+//     } else {
+//       let result = fx(x);
+//       map.set(x, result);
+//       return fx(x);
+//     }
+//   };
+// }
+
+// let momoized = memoizer(heavy);
+
+// momoized(4);
+// momoized(4);
+// momoized(44);
+// momoized(66);
+// momoized(6);
